d65d061e9f605f993070be5d910016013d74f02a
ManashSwain <manash0802@gmail.com>
2025-01-19 21:40:02 +0530

    implemented featured posts

--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -61,7 +61,7 @@ const HomePage = () => {
       {/* Categories and Search bar  */}
 
 
-<div className="flex flex-col lg:flex-row justify-center items-center bg-red-500 p-4 mt-4 rounded-lg gap-4">
+<div className="flex flex-col lg:flex-row justify-center items-center bg-white p-4 mt-4 rounded-lg gap-4">
   {/* Filters Section */}
   <div className="flex flex-wrap justify-center lg:justify-between gap-4 w-full lg:w-3/4">
     <Link to="/posts?cat=all">
@@ -148,15 +148,15 @@ const HomePage = () => {
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Web Design</h3>
+          <Link to="/test"><h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2></Link>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Development</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
@@ -164,7 +164,7 @@ const HomePage = () => {
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Database</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
@@ -172,7 +172,7 @@ const HomePage = () => {
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">Search Engines</h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
