8d0063f5e3b2ab82a8c96c7eda3f158c02de6092
ManashSwain <manash0802@gmail.com>
2025-01-26 10:14:57 +0530

    added introduction section

---
+++ client/src/components/Introduction.jsx
@@ -0,0 +1,15 @@
+const Introduction = () => {
+  return (
+    <>
+    
+    <div className="introduction flex flex-col items-center justify-between gap-4 mt-6 sm:flex-row">
+        <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800 ">Your Destination for creativity, Knowledge and Growth</h1>
+        
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none ">Write a Post</button>
+      </div>
+      <p className="mt-4 text-blue-800 font-medium">Discover insights, tips and trends to fuel your creativity and success.</p>
+    </>
+  )
+}
+
+export default Introduction
--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -1,34 +1,29 @@
 import FeaturedPost from "../components/FeaturedPost";
 import Categories from "../components/Categories";
 import Breadcrumb from "../components/Breadcrumb";
+import Introduction from "../components/Introduction";
 
 const HomePage = () => {
   return (
     <>
       {/* Breadcrumb  */}
 
-     <Breadcrumb/>
+      <Breadcrumb />
 
       {/* Introduction  */}
-
-      <div className="introduction flex flex-col items-center justify-between gap-4 mt-6 sm:flex-row">
-        <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800 ">Your Destination for creativity, Knowledge and Growth</h1>
-        
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none ">Write a Post</button>
-      </div>
-      <p className="mt-4 text-blue-800 font-medium">Discover insights, tips and trends to fuel your creativity and success.</p>
+      <Introduction />
 
       {/* Categories and Search bar  */}
 
-
-      <Categories/>
-
+      <Categories />
 
       {/* Featured post  */}
-      <FeaturedPost/>
-  
+      <FeaturedPost />
+
       {/* Post list  */}
-      <h1 className="text-center text-2xl text-gray-700 font-semibold">Recent Posts</h1>
+      <h1 className="text-center text-2xl text-gray-700 font-semibold">
+        Recent Posts
+      </h1>
     </>
   );
 };
