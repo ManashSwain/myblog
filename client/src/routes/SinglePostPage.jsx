523159efda914495315b69148a9beabfaae8af7e
ManashSwain <manash0802@gmail.com>
2025-01-26 11:13:41 +0530

    changes in single post page

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -1,8 +1,25 @@
+import Image from "../components/Image"
+
 const SinglePostPage = () => {
   return (
    <>
-   <h1>single post page</h1>
-    fr
+   <div className="flex flex-col gap-8">
+    {/* details  */}
+    <div className="flex gap-8">
+      <div className="flex flex-col gap-8 lg:w-3/5">
+
+      </div>
+      <div className=" hidden lg:block w-2/5">
+        <Image src={"profile.jpg"}/>
+      </div>
+    </div>
+    {/* content */}
+    <div>
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+   </div>
+    
    </>
   )
 }
