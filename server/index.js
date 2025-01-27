77165840d35f43ec72afb46f184e4641ce570834
ManashSwain <manash0802@gmail.com>
2025-01-27 21:59:51 +0530

    initialized controllers structure

--- server/index.js
+++ server/index.js
@@ -6,9 +6,13 @@ const port = 3000 ;
 // middlewares 
 app.use(express.json());
 
+
+// requests 
 app.get('/',(req,res)=>{
   res.status(200).send("Hello world!")
 })
+
+
 
 app.listen(port,()=>{
     console.log(`Server is listening on port ${port}`)
