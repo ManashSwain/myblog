12b39389200e0bd13e2db90265c7111b5d91f4d2
ManashSwain <manash0802@gmail.com>
2025-01-27 21:53:26 +0530

    initialized routes structure

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 const port = 3000 ;
 
+// middlewares 
+app.use(express.json());
+
 app.get('/',(req,res)=>{
   res.status(200).send("Hello world!")
 })
