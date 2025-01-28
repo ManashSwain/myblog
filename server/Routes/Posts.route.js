e675abe4077506547ee7e6614370816fd9b2356e
ManashSwain <manash0802@gmail.com>
2025-01-28 21:32:55 +0530

    initialized basic controller

--- server/Routes/Posts.route.js
+++ server/Routes/Posts.route.js
@@ -1,8 +1,7 @@
 import express from "express"
+import { getPosts } from "../Controllers/Posts.controller.js";
 const router = express.Router();
 
-router.get('/posts', (req,res)=>{
-  res.status(200).send("posts");
-})
+router.get('/', getPosts)
 
 export default router
