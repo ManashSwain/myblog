b8f2ae7716dffe4ee2f80d9cbab86bce66fcf28f
ManashSwain <manash0802@gmail.com>
2025-02-01 11:09:08 +0530

    configured webhook for clerk

--- server/Controllers/WebHooks.controller.js
+++ server/Controllers/WebHooks.controller.js
@@ -1,3 +1,25 @@
+import { Webhook } from "svix";
+ 
  export const clerkWebhook = async (req,res)=>{
-    res.status(200).send("Hello clerk route");
+    const WEBHOOKSECRET = process.env.CLERK_SECRET ;
+
+    if(!WEBHOOKSECRET){
+        throw new Error("Webhook secret needed !");
+    }
+    const payload = req.body;
+    const headers = req.headers;
+
+    const wh = new Webhook(WEBHOOKSECRET);
+    let msg;
+    try {
+        msg = wh.verify(payload, headers);
+    } catch (err) {
+        res.status(400).json({
+            message : "Webhook veification failed !"
+        });
+    }
+
+    // Do something with the message...
+
+    res.json({});
 }
--- server/Routes/Webhooks.route.js
+++ server/Routes/Webhooks.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { clerkWebhook } from "../Controllers/WebHooks.controller.js";
+import bodyParser from "body-parser";
 
 const router = express.Router();
 
-router.get("/clerk" , clerkWebhook );
+router.get("/clerk" , bodyParser.raw({ type: 'application/json' })  ,clerkWebhook );
 
 export default router ;
